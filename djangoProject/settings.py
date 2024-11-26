98473f06d5d89df6d586f26e4f6b6cead5851a0c
Julia Semak <julia.semak@outlook.com>
2024-11-26 20:58:45 +0100

    jest logo kurwa w końcu

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / 'templates/static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
